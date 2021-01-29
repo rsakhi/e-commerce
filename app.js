c6501baee9d97afbb03305cb5bffbe9ef6807877
Rsakhi <sakhi.rohit@gmail.com>
2021-01-29 11:27:20 +0530

    added routes and views, installed express and ejs

--- app.js
+++ app.js
@@ -1,8 +1,38 @@
-const http = require('http')
+const path = require('path')
+const fs = require('fs')
 
-const server = http.createServer((req,res) => {
+const expess =  require('express');
+const bodyParser = require('body-parser')
+
+const app = expess();
+app.set('view engine', 'ejs');
+app.set('viwes', './views')
+
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.post('/product', (req,res,next) => {
   console.log(req.url)
-  res.end()
-});
+  console.log(req.body.name)
+  fs.writeFile('./data.json', "dd", (err) => {
+    console.log(err)
+  })
+  res.redirect('/')
+})
 
-server.listen(8081)
+
+app.use('/', (req,res,next) => {
+  console.log(req.url)
+  res.render('layout', {title: 'shop page'})
+  // res.sendFile(path.join(__dirname, 'views', 'product.html'))
+})
+
+
+app.listen(8081)
+
+
+// const server = http.createServer((req,res) => {
+//   console.log(req.url)
+//   res.end()
+// });
+
+// server.listen(8081)
